395513352da61221b303e06564feee8ba26df871
nisrinawafaz <nisrinawafaz@gmail.com>
2023-11-26 00:53:29 +0700

    etalase buku

--- resources/views/page/user/buku/etalaseBuku.blade.php
+++ resources/views/page/user/buku/etalaseBuku.blade.php
@@ -9,7 +9,17 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lilita+One&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/1a24cb4a13.js" crossorigin="anonymous"></script>
     <style>
+        @import url('https://fonts.googleapis.com/css2?family=Dosis:wght@700&display=swap');
+    </style>
+    <style>
+        .text-title {
+            font-family: 'Dosis', sans-serif;
+            margin-top: 15px;
+            font-size: 30px
+        }
+
         .grid-container {
             display: grid;
             grid-template-columns: repeat(4, 1fr);
@@ -23,6 +33,10 @@
             border-radius: 8px;
             box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
             text-align: center;
+            height: 500px;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
         }
 
         .card img {
@@ -36,41 +50,92 @@
             flex-direction: column;
             justify-content: space-between;
         }
+
+        a:link {
+            color: green;
+            background-color: transparent;
+            text-decoration: none;
+        }
+
+
+        a:hover {
+            color: black;
+            background-color: transparent;
+            text-decoration: bold;
+        }
+
+        a:active {
+            color: black;
+            background-color: transparent;
+            text-decoration: underline;
+        }
+
+        .flex-card {
+            display: flex;
+            flex-direction: column;
+            height: 500px;
+            justify-content: space-between;
+        }
     </style>
 </head>
 
-
 <body>
-    <div class="container-fluid">
-        <div class="grid-container">
-            @if ($buku->isEmpty())
-            <p>Tidak ada buku yang tersedia.</p>
-            @else
-            @foreach($buku as $item)
-            <div id="{{ $item->idBuku }}" class="card">
-                <div class="my-flex-container">
-                    <div>
-                    @if ($item->gambar_cover)
-                    <img class="elevation-3 mx-auto" style="width:200px" id="prevImg"
-                        src='{{ asset($item->gambar_cover) }}' style="display: block;" />
-
-                    @else
-                    <img class="elevation-3 mx-auto" id="prevImg"
-                        src="{{ asset('storage/admin/cover_buku/boxed-bg.jpg') }}" width="300px"
-                        style="display: block;" />
-                    @endif
-                    </div>
-                    <div>
-                    <h4 class="card-title" id="nama-${menu.num}">{{ $item->judul }}</h4>
-                    <p class="card-text">{{ $item->genre->nama_genre }}</p>
-                    <button type="button" class="btn btn-success" id="${menu.num}" onclick='tambahOrder(this)'>Tambah
-                        Barang</button>
-                    </div>
-                </div>
-            </div>
-            @endforeach
-            @endif
+    <nav class="navbar navbar-expand-lg bg-white fixed-top navbar-scroll">
+        <div class="container">
+            <ul class="nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Catalog</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Profile</a>
+                </li>
+            </ul>
+            <a class="navbar-brand" href="#">KOLEKSI KATA</a>
+            <ul class="nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Favorite</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">My Book</a>
+                </li>
+            </ul>
         </div>
+    </nav>
+    <div class="input-group rounded " style="width:400px">
+        <input type="search" class="form-control rounded  me-2" placeholder="Search" aria-label="Search"
+            aria-describedby="search-addon" />
+        <span class="input-group-text border-0" id="search-addon">
+            <i class='fas fa-search'></i>
+        </span>
+    </div>
+    <img class="elevation-3 mx-auto" style="width:100%" id="prevImg"
+                            src='{{ asset('storage/admin/image/book.png') }}' style="display: block;" />
+    <div class="grid-container">
+        @if ($buku->isEmpty())
+        <p>Tidak ada buku yang tersedia.</p>
+        @else
+        @foreach($buku as $item)
+        <div id="{{ $item->idBuku }}" class="card">
+            <div>
+                @if ($item->gambar_cover)
+                <img class="elevation-3 mx-auto" style="width:200px" id="prevImg" src='{{ asset($item->gambar_cover) }}'
+                    style="display: block;" />
+
+                @else
+                <img class="elevation-3 mx-auto" id="prevImg" src="{{ asset('storage/admin/cover_buku/boxed-bg.jpg') }}"
+                    width="300px" style="display: block;" />
+                @endif
+
+                <h4 class="text-title" id="nama-${menu.num}">'{{ $item->judul }}'</h4>
+                <p class="card-text">{{ $item->genre->nama_genre }}</p>
+                <p class="card-text">{{$item->harga_harian }}</p>
+            </div>
+            <div>
+                <button type="button" class="btn btn-success" id="${menu.num}" onclick='tambahOrder(this)'>Sewa</button>
+            </div>
+        </div>
+        @endforeach
+        @endif
     </div>
 </body>
 
