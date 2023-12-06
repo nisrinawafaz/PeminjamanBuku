ddd597a2802a6181294666d3ec1ac84907942721
nisrinawafaz <nisrinawafaz@gmail.com>
2023-12-06 08:42:38 +0700

    bnrin controller ke history peminjaman

--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,7 @@ Route::get('/', function () {
 Auth::routes();
 Route::get('/etalaseBuku', [BukuController::class, 'etalaseBuku'])->name('etalaseBuku');
 
-Route::controller(SewaController::class)
+Route::controller(HistoryPeminjamanController::class)
     ->prefix('sewa')
     ->as('sewa.')
     ->group(function () {
